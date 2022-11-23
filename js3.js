30214c57a13b049e2277391f4820e62bf22b8353
JovanNaumov <115021955+JovanNaumov@users.noreply.github.com>
2022-11-23 18:06:58 +0100

    Add files via upload

---
+++ js3.js
@@ -0,0 +1,68 @@
+//Hello World
+
+var code = "ru"
+   if(code == "ru") {
+console.log("Здравствуй, мир!");
+} else if(code == "de") {
+    console.log("Hallo Welt");
+} else if(code == "MK") {
+    console.log("Здраво свету");
+}else {
+    console.error("Language not found.");
+}
+// Seasons 
+
+// var seasons = prompt("Enter the month to chek the sesson");
+//     if(seasons == "September" || seasons == "september" || seasons == "October" || seasons == "october" || seasons == "November" || seasons == "november"){
+
+//         console.log("The season is Autumn");
+//     }else if(season == "december" || season == "january" || season == "february"){
+//         console.log("The season is winter.");
+//     }else if(Season == "march" || Season == "april " || Season == "may"){
+//         console.log("The season is Spring.");
+//     }else if(Season == "june" || Season == "july" || Season == "august"){
+//         console.log("The season is Summer.");
+//     }else {
+//         console.log("Try Again");
+//     }
+
+
+    //Musicians
+
+    var band_members_num = 1;
+
+    switch ( band_members_num){
+
+    case 1:
+        console.log("This is a solo band");
+    break;
+
+    case 2:
+        console.log("This is a duet");
+    break;
+    
+    case 3:
+        console.log("This is a 3man squad");
+    break;
+    
+    case 4:
+        console.log("This is an orchestra");
+    break;
+
+    case 5:
+        console.log("This is an orchestra");
+    break;
+
+    case 6:
+        console.log("This is an orchestra");
+    break;
+
+    default: console.log("Unknown Band");
+    }
+
+
+
+
+
+
+
